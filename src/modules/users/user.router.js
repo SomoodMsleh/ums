26bced0e45b0e6492b40052b561924ef097a117f
SomoodMsleh <somoodmedwan@gmail.com>
2025-01-14 19:33:56 +0200

    create login end point

--- src/modules/users/user.router.js
+++ src/modules/users/user.router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import bcryptjs from 'bcryptjs'
 import userModel from "../../../DB/model/user.model.js";
+import { where } from "sequelize";
 const router = Router();
 
 router.get('/', async(req,res)=>{
@@ -16,5 +17,19 @@ router.post('/',async (req,res)=>{
     return res.status(201).json({massage:"successfully"});
 });
 
+router.post('/login',async (req,res)=>{
+    const {email,password} = req.body;
+    const user = await userModel.findOne({
+        where :{email:email}
+    });
+    if(user == null){
+        return res.status(404).json({massage:"invalid email"});
+    }
+    const check = await bcryptjs.compareSync(password,user.password);
+    if(check == false){
+        return res.status(400).json({massage:"invalid password"});
+    }
+    return res.status(200).json({massage:"successfully" ,user});
+});
 
 export default router;
